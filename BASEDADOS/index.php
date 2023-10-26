81f18393ee47ccc724bba07554008ed018ecd436
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2023-10-26 09:43:35 +0200

    Add files via upload

---
+++ BASEDADOS/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- bootstrap -->
+</head>
+<body>
+    
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link"  href="#">Início</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Quem somos?</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container ">
+          <h1><span>CESVEL</span> CONSULTORES</h1>
+          <p class="lead">Inovação através da consultoria inteligente.</p>
+        </div>
+      </div>
+
+      <div class="container">
+      <p class="h1"> Serviçios prestados</p> 
+        <section class="cards-container">
+
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <div class="card">
+                <img src="imgs/kevin-ku-w7ZyuGYNpRQ-unsplash.jpg" alt="" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum officiis iure dolore odio, quod repellendus itaque natus ex! Rem debitis esse dolores nostrum tempora distinctio quod repellendus aut, sequi vero.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-4">
+              <div class="card">
+                <img src="imgs/desola-lanre-ologun-IgUR1iX0mqM-unsplash.jpg" alt="" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ea. Aut expedita illum accusamus reiciendis inventore fugit veritatis, consequatur doloribus officiis! Qui excepturi numquam voluptas. Quidem, qui. Vitae, laudantium nemo?</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-4">
+              <div class="card">
+                <img src="" alt="" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+
+
+      <!-- bootstrap  -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      <!-- bootstrap  -->
+
+      <?php 
+
+?>
+</body>
+</html>
