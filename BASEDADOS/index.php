432cc878b8b14aed64ae99a47052823470a5ecaa
telesshelton <stelesu21@gmail.com>
2023-10-29 14:14:12 +0200

    Actualizações feitas

--- BASEDADOS/index.php
+++ BASEDADOS/index.php
@@ -11,10 +11,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- bootstrap -->
 </head>
-<body>
+<body class="body">
     
-    <nav>
+    <nav class="bg-white">
         <ul class="nav">
+        <a class="navbar-brand" href="#">
+      <img src="imgs/cesvel.jpg" alt="Avatar Logo" style="width:100px;" class=" "> 
+    </a>
             <li class="nav-item">
               <a class="nav-link"  href="#">Início</a>
             </li>
@@ -29,7 +32,9 @@
 
     <div class="jumbotron jumbotron-fluid">
         <div class="container ">
-          <h1><span>CESVEL</span> CONSULTORES </h1>
+        <!-- <img src="imgs/cesvel.jpg" mx-auto d-bloc  width="10%" height=""  alt=""> -->
+          <h1 class="display-1"><span>CESVEL</span> CONSULTORES </h1>
+          
           <p class="lead">Inovação através da consultoria inteligente.</p>
         </div>
       </div>
@@ -41,7 +46,7 @@
           <div class="row">
             <div class="col-12 col-sm-4">
               <div class="card">
-                <img src="imgs/kevin-ku-w7ZyuGYNpRQ-unsplash.jpg" alt="" class="card-img-top">
+                <img src="imgs/1.jpg" alt="" class="card-img-top">
                 <div class="card-body">
                   <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum officiis iure dolore odio, quod repellendus itaque natus ex! Rem debitis esse dolores nostrum tempora distinctio quod repellendus aut, sequi vero.</p>
                 </div>
@@ -49,25 +54,42 @@
             </div>
             <div class="col-12 col-sm-4">
               <div class="card">
-                <img src="imgs/desola-lanre-ologun-IgUR1iX0mqM-unsplash.jpg" alt="" class="card-img-top">
+                <img src="imgs/2.jpg" alt="" class="card-img-top">
                 <div class="card-body">
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ea. Aut expedita illum accusamus reiciendis inventore fugit veritatis, consequatur doloribus officiis! Qui excepturi numquam voluptas. Quidem, qui. Vitae, laudantium nemo?</p>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum officiis iure dolore odio, quod repellendus itaque natus ex! Rem debitis esse dolores nostrum tempora distinctio quod repellendus aut, sequi vero.</p>
                 </div>
               </div>
             </div>
             <div class="col-12 col-sm-4">
               <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="imgs/3.jpg" alt="" class="card-img-top">
                 <div class="card-body">
-                  <p class="card-text"></p>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum officiis iure dolore odio, quod repellendus itaque natus ex! Rem debitis esse dolores nostrum tempora distinctio quod repellendus aut, sequi vero.</p>
                 </div>
               </div>
             </div>
           </div>
         </section>
+
+
+        <section class="cards-containerh2">
+
+      <article>
+        <h2>Careira</h2>
+        <p>paragrafo 1</p>
+        <p>paragrafo 2</p>
+      </article> 
+
+      </section>
+
+
       </div>
 
-
+    <footer class = "Rodape bg-dark-subtle">
+      <div class="container">
+        <p>Contacte-nos</p>
+      </div>    
+    </footer>
 
       <!-- bootstrap  -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
